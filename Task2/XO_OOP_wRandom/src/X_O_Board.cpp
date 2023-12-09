3493c80e439bb70e336907f50abfaa5673345c78
Cekymeky <islamyasser840@gmail.com>
2023-12-09 10:09:24 +0200

    pyramic xo finished --random player need to be updated

--- Task2/XO_OOP_wRandom/src/X_O_Board.cpp
+++ Task2/XO_OOP_wRandom/src/X_O_Board.cpp
@@ -2,6 +2,7 @@
 // Author:  Mohammad El-Ramly
 // Date:    10/10/2022
 // Version: 1
+#include <bits/stdc++.h>
 #include <iostream>
 #include <random>
 #include <iomanip>
@@ -10,22 +11,27 @@
 using namespace std;
 
 // Set the board
-X_O_Board::X_O_Board () {
-   n_rows = n_cols = 3;
-   board = new char*[n_rows];
-   for (int i = 0; i < n_rows; i++) {
-      board [i] = new char[n_cols];
-      for (int j = 0; j < n_cols; j++)
-         board[i][j] = 0;
+X_O_Board::X_O_Board()
+{
+   n_rows = 3;
+   n_cols = 5;
+   board = vector<vector<char>>(n_rows);
+
+   for (int i = 1; i < n_rows + 1; ++i)
+   {
+      vector<char> v(2 * i - 1, 0);
+      board[i - 1] = v;
    }
 }
 
 // Return true  if move is valid and put it on board
 // within board boundaries in empty cell
 // Return false otherwise
-bool X_O_Board::update_board (int x, int y, char mark){
+bool X_O_Board::update_board(int x, int y, char mark)
+{
    // Only update if move is valid
-   if (!(x < 0 || x > 2 || y < 0 || y > 2) && (board[x][y] == 0)) {
+   if (!(x < 0 || x > 2 || y < 0 || y > 2 * x + 1) && (board[x][y] == 0))
+   {
       board[x][y] = toupper(mark);
       n_moves++;
       return true;
@@ -35,46 +41,66 @@ bool X_O_Board::update_board (int x, int y, char mark){
 }
 
 // Display the board and the pieces on it
-void X_O_Board::display_board() {
-   for (int i: {0,1,2}) {
-      cout << "\n| ";
-      for (int j: {0,1,2}) {
+void X_O_Board::display_board()
+{
+   for (int i : {0, 1, 2})
+   {
+      for (int k = 0; k < 8.25 * (n_rows - i); ++k)
+         cout << " ";
+      cout << "| ";
+      for (int j = 0; j < board[i].size(); ++j)
+      {
          cout << "(" << i << "," << j << ")";
-         cout << setw(2) << board [i][j] << " |";
+         cout << setw(2) << board[i][j] << "| ";
       }
-      cout << "\n-----------------------------";
+      cout << "\n\n";
    }
-   cout << endl;
+   cout << "--------------------------------------------------------------\n";
 }
 
 // Returns true if there is any winner
 // either X or O
-// Written in a complex way. DO NOT DO LIKE THIS.
-bool X_O_Board::is_winner() {
-    char row_win[3], col_win[3], diag_win[2];
-    for (int i:{0,1,2}) {
-        row_win[i] = board[i][0] & board[i][1] & board[i][2];
-        col_win[i] = board[0][i] & board[1][i] & board[2][i];
-    }
-    diag_win[0] = board[0][0] & board[1][1] & board[2][2];
-    diag_win[1] = board[2][0] & board[1][1] & board[0][2];
+bool X_O_Board::is_winner()
+{
+   char diag_win[2];
 
-    for (int i:{0,1,2}) {
-        if ( (row_win[i] && (row_win[i] == board[i][0])) ||
-             (col_win[i] && (col_win[i] == board[0][i])) )
-            {return true;}
-    }
-    if ((diag_win[0] && diag_win[0] == board[1][1]) ||
-        (diag_win[1] && diag_win[1] == board[1][1]))
-        {return true;}
-    return false;
+   //  diagonal wins
+   diag_win[0] = (board[0][0] == board[1][2]) && (board[1][2] == board[2][4]) && (board[2][4] != 0);
+   diag_win[1] = (board[2][0] == board[1][0]) && (board[1][0] == board[0][0]) && (board[0][0] != 0);
+
+   if (diag_win[0] || diag_win[1])
+   {
+      return true;
+   }
+
+   //  horizontal wins
+   char hor_win[4];
+   hor_win[0] = (board[1][0] == board[1][1]) && (board[1][1] == board[1][2]) && (board[1][2] != 0);
+   hor_win[1] = (board[2][0] == board[2][1]) && (board[2][1] == board[2][2]) && (board[2][2] != 0);
+   hor_win[2] = (board[2][1] == board[2][2]) && (board[2][2] == board[2][3]) && (board[2][3] != 0);
+   hor_win[3] = (board[2][2] == board[2][3]) && (board[2][3] == board[2][4]) && (board[2][4] != 0);
+
+   if (hor_win[0] || hor_win[1] || hor_win[2] || hor_win[3])
+   {
+      return true;
+   }
+   //  vertical win
+
+   if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && (board[2][2] != 0))
+   {
+      return true;
+   }
+
+   return false;
 }
 
 // Return true if 9 moves are done and no winner
-bool X_O_Board::is_draw() {
-    return (n_moves == 9 && !is_winner());
+bool X_O_Board::is_draw()
+{
+   return (n_moves == 9 && !is_winner());
 }
 
-bool X_O_Board::game_is_over () {
-    return n_moves >= 9;
+bool X_O_Board::game_is_over()
+{
+   return n_moves >= 9;
 }
